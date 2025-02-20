4877ead9b0a190a150f0b065c29100c64d9addc4
Peter <szpeto12@gmail.com>
2025-02-20 21:33:43 +0100

    Final working version v2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
 
 public class Main {
 
+	public static void main(String [] args) {
+		
+		
+		
+	}
+	
+	public boolean sum28(int[] nums) {
+		  
+		if(nums.length <= 1) return false;
+		int result = 0;
+		
+		for(int i = 0; i<nums.length; i++) {
+			
+			if(nums[i] == 2) result = result + 2;
+			
+		}
+		
+		if(result == 8) return true;
+		return false;
+		
+	}
+
+	
 }
